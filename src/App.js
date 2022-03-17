80d7babdf95edac12ea36d14c87289ebc6368d2d
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2022-03-17 18:49:59 +0100

    imitial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Gallery from './Gallery'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Image Gallery</h1>
+        <h2>Author: Aleksandra Wolska</h2>
+      </header>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
