76c7ec16fbeff81e691dde49f875f40336949294
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-01-01 20:06:52 +0100

    added contact email

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
             <header className="header">
                 <h1>Photography</h1>
                 <h2>Author: Aleksandra Wolska</h2>
+                <h2 id="contact-h2">Contact: aleksandra.joanna.wolska@gmail.com</h2>
                 <a href="https://github.com/AleksandraWolska/photo-gallery.io">
                     <img className="seecode" src={seecode} alt="See source code" />
                 </a>
